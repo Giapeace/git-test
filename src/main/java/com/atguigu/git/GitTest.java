f04865f37f7920b880bb905e339649e172e01d03
giapeace <giapeace@qq.com>
2021-08-10 13:42:35 +0800

    second commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,14 @@
+package main.java.com.atguigu.git;
+
+/**
+ * @author LJNING
+ * @create 2021-08-10 13:26
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+        System.out.println("hello git2!");
+
+    }
+}
